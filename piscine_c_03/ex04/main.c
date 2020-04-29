0edd65d1649785504987da58e1cd54c316c8f828
Antaragorn <Antaragorn@gmail.com>
2020-04-29 22:50:19 +0200

    xd

--- piscine_c_03/ex04/main.c
+++ piscine_c_03/ex04/main.c
@@ -4,7 +4,7 @@ char	*ft_strstr(char *str, char *to_find);
 int main ()
 {
   char str[] ="El cielo esta enladrillado y me fumao 3 canutos"; 
-  puts (ft_strstr (str,"ca"));
+  puts (ft_strstr (str,"me"));
 
   return 0;
 }
